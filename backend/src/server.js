7448282c8d436b07c280cd09b0f0fa36971e5c85
João Victor Ferreira Silva <joaovictorferrari4@gmail.com>
2024-05-21 22:46:34 -0300

    disable csp

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,12 +14,15 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-
 // Rotas
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
+app.use((req, res, next) => {
+    //res.setHeader("Content-Security-Policy", "default-src 'none'; font-src 'self' data:");
+    next();
+});
 
 // Conectar ao MongoDB
 mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
